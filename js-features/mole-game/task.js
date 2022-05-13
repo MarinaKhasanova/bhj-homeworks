702c9724250868336c61bc41811d389058134a7f
MarinaKhasanova <mnmatis@gmail.com>
2022-05-13 08:53:31 +0300

    task 3

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,3 +1,27 @@
-
+const deadStatus = document.getElementById('dead');
+const lostStatus = document.getElementById ('lost');
 
 getHole = index => document.getElementById(`hole${index}`);
+
+for (let i = 1; i <= 9; i ++) {
+  let hole = getHole(i);
+  console.log(hole); 
+  hole.onclick = function () {
+    if (hole.className.includes( 'hole_has-mole' )) {
+      deadStatus.textContent ++; 
+    } else {
+      lostStatus.textContent ++;
+      }
+    if (deadStatus.textContent == 10) {
+      deadStatus.textContent = 0;
+      lostStatus.textContent = 0;
+      alert('Вы победили!');
+      
+    } 
+    if (lostStatus.textContent == 5) {
+      deadStatus.textContent = 0;
+      lostStatus.textContent = 0;
+      alert('Вы проиграли (');
+    }
+  }  
+}
