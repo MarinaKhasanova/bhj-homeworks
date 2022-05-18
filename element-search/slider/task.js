b01037724852d3c380a8e55a1d2a344ccff2a88d
MarinaKhasanova <mnmatis@gmail.com>
2022-05-18 18:42:56 +0300

    task3

---
+++ element-search/slider/task.js
@@ -0,0 +1,48 @@
+const prev = document.querySelector(".slider__arrow_prev");
+const ridht = document.querySelector(".slider__arrow_next");
+const slider = document.querySelectorAll(".slider__item");
+const dot = document.querySelectorAll(".slider__dot");
+let i = 0; 
+
+const activeSlide = n => {
+  for (slide of slider) {
+	slide.classList.remove("slider__item_active");
+  }
+  slider[n].classList.add("slider__item_active");
+}
+
+const getCurrentSlide = function (idx) {
+  activeSlide(idx);
+  dotActive(idx);
+}
+
+const nextSlide = function () {
+  if (i === slider.length - 1) {
+    i = 0;
+	getCurrentSlide(i);
+  } else {
+      i++;
+	  getCurrentSlide(i);
+	}
+}
+
+const prevSlide = function () {
+  if (i === 0) {
+    i = slider.length - 1;
+	getCurrentSlide(i);
+  } else {
+      i--;
+	  getCurrentSlide(i);
+	}
+}
+
+
+ridht.onclick = function () {
+  nextSlide();
+  return false;
+}
+
+prev.onclick = function () {
+  prevSlide();
+  return false;
+}
