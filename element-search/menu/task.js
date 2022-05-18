ecfdd0e128f85d9b2fcbcee5e269f7fd262ae11e
MarinaKhasanova <mnmatis@gmail.com>
2022-05-18 18:32:47 +0300

    task 2 finalized

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,21 +1,17 @@
-let classLinks = document.querySelectorAll(".menu__link");//обращаемся как в сss
+const classMenuLinks = document.querySelectorAll(".menu__link");
 
-for (let menuLink of classLinks) {
-    menuLink.onclick = function () {
-        let menuLinkParent = menuLink.closest("li");
-    let activeMenuItem = document.querySelector(".menu_active");
-
-    let menuSub = menuLinkParent.querySelector(".menu__item > .menu_sub");
-        let childrenMenuSub = menuLinkParent.querySelector(".menu_active");
-
-        if (activeMenuItem !== null) {
-            activeMenuItem.classList.remove("menu_active");
-        }
-
-        if (menuSub !== null && childrenMenuSub === null) {
-            menuSub.classList.toggle("menu_active");
-        }
-
-        return false;
-    };
+for (let menuLink of classMenuLinks) {
+  menuLink.onclick = function () {
+    const menuLinkParent = menuLink.closest("li");
+    const activeMenuItem = document.querySelector(".menu_active");
+    const menuSub = menuLinkParent.querySelector(".menu__item > .menu_sub");
+    const childrenMenuSub = menuLinkParent.querySelector(".menu_active");
+      if (activeMenuItem !== null) {
+        activeMenuItem.classList.remove("menu_active");
+       }
+      if (menuSub !== null && childrenMenuSub === null) {
+        menuSub.classList.toggle("menu_active");
+      }
+    return false;
+  };
 };
