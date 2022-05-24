8d2977f96845583ec761f0b121bb54be2077fc78
MarinaKhasanova <mnmatis@gmail.com>
2022-05-24 14:17:17 +0300

    task 2 final version

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,17 +1,12 @@
-const classMenuLinks = document.querySelectorAll(".menu__link");
+const menuLinks = document.querySelectorAll('.menu__link');
 
-for (let menuLink of classMenuLinks) {
-  menuLink.onclick = function () {
-    const menuLinkParent = menuLink.closest("li");
-    const activeMenuItem = document.querySelector(".menu_active");
-    const menuSub = menuLinkParent.querySelector(".menu__item > .menu_sub");
-    const childrenMenuSub = menuLinkParent.querySelector(".menu_active");
-      if (activeMenuItem !== null) {
-        activeMenuItem.classList.remove("menu_active");
-       }
-      if (menuSub !== null && childrenMenuSub === null) {
-        menuSub.classList.toggle("menu_active");
-      }
+for(let menuLink of menuLinks) {
+  const menuSub = menuLink.closest('li').querySelector('ul');
+  if (!menuSub) {
+    continue;
+  }
+  menuLink.onclick = function() {
+    menuSub.classList.toggle("menu_active");
     return false;
-  };
-};
+  }
+}
