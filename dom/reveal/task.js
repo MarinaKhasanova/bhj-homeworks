3d08c44aa4ca52b2b30b4fa1f023471cfe7c3fe3
MarinaKhasanova <mnmatis@gmail.com>
2022-07-28 15:19:12 +0300

    task1

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -2,23 +2,12 @@ const reveals = document.getElementsByClassName("reveal");
 
 document.addEventListener('scroll', function() {
   for (let reveal of reveals) {
-    if (reveal) {
-      reveal.classList.toggle('reveal_active');
+    const viewportHeight = window.innerHeight;
+    const reveal_top = reveal.getBoundingClientRect().top;
+    if (reveal_top < viewportHeight && reveal_top > 0) {
+      reveal.classList.add("reveal_active");
+    } else {
+      reveal.classList.remove("reveal_active");
     }
-  }
+  } 
 });
-
-
-/*
-
-document.addEventListener('scroll', function() {
-    for (let reveal of reveals) {
-        if (reveal) {
-            let top = reveal.getBoundingClientRect().top;//возвращает размер элемента и его позицию относительно viewport
-            const view = window.innerHeight;//фиксируем высоту элемента относительно страницы
-            if (top < view) reveal.classList.add("reveal_active");//если высота элемента меньше экр показываем элемент
-            else reveal.classList.remove("reveal_active");//если нет скрываем
-        }
-    }
-});
-*/
