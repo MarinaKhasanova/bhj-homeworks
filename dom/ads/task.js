c8c8f372f44f86c7b8d14f584d1b136528cb9947
MarinaKhasanova <mnmatis@gmail.com>
2022-07-31 11:53:34 +0300

    ads realized

---
+++ dom/ads/task.js
@@ -0,0 +1,15 @@
+const rotator = document.getElementsByClassName('rotator__case');
+let i=1;
+
+setInterval(() => {
+  if (i == 0) {
+    rotator[rotator.length-1].classList.remove("rotator__case_active");
+    rotator[i].classList.add("rotator__case_active");
+  }  
+  else {
+    rotator[i-1].classList.remove("rotator__case_active");
+    rotator[i].classList.add("rotator__case_active");
+  }
+  if ( i<rotator.length-1) {i++;} 
+  else {i=0;}
+}, 1000)
