cee367d8e17db700658bb13509411b8adf5909ce
MarinaKhasanova <mnmatis@gmail.com>
2022-08-12 10:14:00 +0300

    доработана задача tooltip

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -5,9 +5,16 @@ element.setAttribute('style', 'left: 0; top: 0');
 
 for (let help of helps) {
   help.onclick = () => {
+    if (element.textContent === help.getAttribute('title')) {
+      element.classList.toggle('tooltip_active');
+      return false;
+    } else {
+        element.classList.add('tooltip_active');
+    }
     element.textContent = help.getAttribute('title');
-    console.log(element.textContent);
-    element.classList.add('tooltip_active');
+    let positionHelp = help.getBoundingClientRect();
+    element.style.left = Math.floor(positionHelp.left) + 'px';
+    element.style.top = Math.floor(positionHelp.top + 20) + 'px';
     help.insertAdjacentElement( "afterend", element);
     return false;
   };
